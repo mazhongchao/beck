4800abe59940bae1b0e86675e00be27c85b21d5b
mazhongchao <mazhongchao@gmail.com>
2023-01-05 13:50:04 +0800

    modify template.class.php

--- sys/lib/template.class.php
+++ sys/lib/template.class.php
@@ -5,19 +5,18 @@ class Template
     protected $config = [
         'path' => '',
     ];
-
-
     protected $data = [];
 
     public function __construct(array $config) {
         $this->config = array_merge($this->config, $config);
     }
 
-    public function assign(array $data) {
-        $this->data = array_merge($this->data, $data) ;
+    public function render($tpl, array $data = []) {
+        $output = $this->fetch($tpl, $data);
+        echo $output;
     }
 
-    public function fetch($tpl, array $data = []) {
+    private function fetch($tpl, array $data = []) {
         $this->assign($data);
 
         ob_start();
@@ -27,15 +26,13 @@ class Template
         $content = ob_get_clean();
         ob_end_clean();
         return $content;
-
     }
 
-    public function render($tpl, array $data = []) {
-        $output = $this->fetch($tpl, $data);
-        echo $output;
+    private function assign(array $data) {
+        $this->data = array_merge($this->data, $data);
     }
 
     private function get_template($tpl) {
-        //
+        return $this->config['path'].'/'.$tpl;
     }
 }
