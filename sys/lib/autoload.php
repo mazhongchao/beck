f0010ec07a17e2b121ccf71df57b7be81c447415
mazhongchao <mazhongchao@gmail.com>
2023-01-23 10:30:09 +0800

    update autoload and remove and rename files

--- sys/lib/autoload.php
+++ sys/lib/autoload.php
@@ -1,7 +1,7 @@
 <?php
 function class_autoloader($class_name) {
     $class_name = strtolower($class_name);
-    $class_file = __DIR__.'/' . $class_name . '.class.php';
+    $class_file = __DIR__ . DIRECTORY_SEPARATOR . $class_name . '.php';
 
     if (isset($class_file) && file_exists($class_file)) {
         include $class_file;
