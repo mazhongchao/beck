9422917bdd19fbb31e4de3f0100a4ff20fb5b972
mazhongchao <mazhongchao@gmail.com>
2023-01-26 14:41:50 +0800

    Directory structure adjustment

--- sys/base.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('APP_DIR', dirname(dirname(__FILE__)));
-define('SYS_DIR', dirname(__FILE__));
-define('LIB_DIR', dirname(__FILE__)."/lib");
-
-require LIB_DIR."/autoload.php";
-session_start();
