c4947d72aaba17c1a00ade59ab8a641ae52cfdf2
mazhongchao <mazhongchao@gmail.com>
2023-03-02 21:52:52 +0800

    update

--- adm/article-list.php
+++ adm/article-list.php
@@ -1,1 +1,7 @@
 <?php
+$query_data = Url::queryData();
+
+$do = new DataObject('../test/beck.db');
+
+$artile_list = $do::articles($query_data['page'], $query_data['per_page'], $query_data['filter']);
+
---
+++ adm/lib/url.php
@@ -0,0 +1,20 @@
+<?php
+class Url
+{
+    public static function queryData(): array {
+        $query_data = ['page'=>1, 'per_page'=>30, 'filter'=>null];
+
+        if (isset($_REQUEST['page'])) {
+            $query_data['page'] = $_REQUEST['page'];
+        }
+        if (isset($_REQUEST['per_page'])) {
+            $query_data['per_page'] = $_REQUEST['per_page'];
+        }
+        if (isset($_REQUEST['filter'])) {
+            //filter_arr = [];
+            $filter_arr = explode('&', $_REQUEST['filter']);
+            $query_data['filter'] = $filter_arr;
+        }
+        return $query_data;
+    }
+}
