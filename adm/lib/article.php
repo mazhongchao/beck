d61dd4d4dfe4d76c9484ae02742dc8c614a7e07f
mazhongchao <mazhongchao@gmail.com>
2023-03-04 21:56:03 +0800

    add class Article

---
+++ adm/lib/article.php
@@ -0,0 +1,49 @@
+<?php
+class Article
+{
+    public static function save()
+    {
+        //
+    }
+
+    public static function preview()
+    {
+        //
+    }
+
+    public static function publish()
+    {
+        //
+    }
+
+    public static function remove()
+    {
+        //
+    }
+
+    public static function edit()
+    {
+        //
+    }
+
+    public static function update()
+    {
+        //
+    }
+
+    public static function query($page=1, $page_size=30, $filter=null): array
+    {
+        $start = ($page - 1) * $page_size;
+        $rows = $page_size;
+
+        $limit = "{$start},{$rows}";
+        $res = self::query("bk_article", "*", $filter, null, "", "", "",
+            $limit);
+
+        $list = [];
+        while ($row = $res->fetchArray()) {
+            array_push($data, $row);
+        }
+        return $list;
+    }
+}
