dea838c3ad36ad64f2aaa246e6435459c9388df3
mazhongchao <mazhongchao@gmail.com>
2023-03-15 15:04:16 +0800

    update base.php

--- adm/base.php
+++ adm/base.php
@@ -3,9 +3,7 @@ define('APP_DIR', dirname(__FILE__, 2) .DIRECTORY_SEPARATOR);
 define('ADM_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
 const LIB_DIR = ADM_DIR . "lib" . DIRECTORY_SEPARATOR;
-# const DB_FILE = ADM_DIR . "data". DIRECTORY_SEPARATOR . "beck.db";
-
-const DB_FILE = ADM_DIR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "test/beck.db";
+const DB_FILE = ADM_DIR . "data". DIRECTORY_SEPARATOR . "beck.db";
 
 require_once LIB_DIR."autoload.php";
 session_start();
