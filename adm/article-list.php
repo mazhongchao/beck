564942076da7a7582641ca3aba6211b865809b77
mazhongchao <mazhongchao@gmail.com>
2023-03-16 18:28:03 +0800

    update article-list

--- adm/article-list.php
+++ adm/article-list.php
@@ -1,7 +1,6 @@
 <?php
+require 'base.php';
+
 $query_data = Url::queryData();
-
-$do = new DataObject('../test/beck.db');
-
-$artile_list = $do::articles($query_data['page'], $query_data['per_page'], $query_data['filter']);
+$list = Article::show($query_data['page'], $query_data['per_page'], $query_data['filter']);
 
