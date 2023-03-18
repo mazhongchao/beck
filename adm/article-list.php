5c755ca4e5021c759d51ac78ce187b4fea576b6c
mazhongchao <mazhongchao@gmail.com>
2023-03-19 07:34:17 +0800

    update

--- adm/article-list.php
+++ adm/article-list.php
@@ -2,5 +2,5 @@
 require 'base.php';
 
 $query_data = Url::queryData();
-$list = Article::show($query_data['page'], $query_data['per_page'], $query_data['filter']);
+$list = Article::list($query_data['page'], $query_data['per_page'], $query_data['filter']);
 
