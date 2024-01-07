3374627ec30ce050aaf247bc8b58df0c45de9265
mazhongchao <mazhongchao@gmail.com>
2024-01-07 22:22:14 +0800

    update base

--- adm/base.php
+++ adm/base.php
@@ -3,7 +3,16 @@ define('APP_DIR', dirname(__FILE__, 2) .DIRECTORY_SEPARATOR);
 define('ADM_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
 const LIB_DIR = ADM_DIR . "lib" . DIRECTORY_SEPARATOR;
-const DB_FILE = ADM_DIR . "data". DIRECTORY_SEPARATOR . "beck.db";
+const DB_DIR = ADM_DIR . "data". DIRECTORY_SEPARATOR;
+
+const DB_FILE = DB_DIR . DIRECTORY_SEPARATOR . "beck.db";
+
+#const DB_FILE = ADM_DIR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "test/beck.db";
 
 require_once LIB_DIR."autoload.php";
+
+if (!file_exists(DB_FILE)) {
+    $db = new Database(DB_FILE);
+    $db->init();
+}
 session_start();
